89677f680d7e1290114d0475c711c364d9767153
khale1997 <70625271+khale1997@users.noreply.github.com>
2024-04-03 20:50:31 -0400

    Audio Successfully Tested

--- galagaSurfers/Assets/AudioManager.cs
+++ galagaSurfers/Assets/AudioManager.cs
@@ -5,19 +5,71 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public AudioSource audioSource;
+    public AudioSource musicSource;
+    public AudioSource soundFXSource;
+
+    public bool isMenuMusic = true;
 
     public AudioClip menu;
+    public AudioClip background;
+    public AudioClip explode;
+    public AudioClip buttonClick;
+    public AudioClip bulletHit;
+
+    public float soundVolume = 1.0f;
+    
     void Start()
     {
-        audioSource.clip = menu;
-        audioSource.loop = true;
-        audioSource.Play();
+        musicSource.volume = soundVolume;
+        soundFXSource.volume = soundVolume;
+
+        musicSource.clip = menu;
+        musicSource.loop = true;
+        musicSource.Play();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        // menu and back ground music
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            if (isMenuMusic) {
+                musicSource.clip = background;
+                musicSource.loop = true;
+                musicSource.Play();
+                isMenuMusic = false;
+            } else {
+                musicSource.clip = menu;
+                musicSource.loop = true;
+                musicSource.Play();
+                isMenuMusic = true;
+            }
+        }
+
+        // bullet hit
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            soundFXSource.clip = bulletHit;
+            soundFXSource.Play();
+        }
+
+        // explode
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            soundFXSource.clip = explode;
+            soundFXSource.Play();
+        }
+
+        // button click
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            soundFXSource.clip = buttonClick;
+            soundFXSource.Play();
+        }
+
+        // stop the music
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            if (musicSource.isPlaying)
+            {
+                musicSource.Stop();
+            }
+        }
+    }   
 }
